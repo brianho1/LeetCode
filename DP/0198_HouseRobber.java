c93959fa39094c64f1a34fbaff5c44f22b5618da
hobr86 <hobr86@gmail.com>
2020-08-03 15:00:29 -0500

    DP
    DP

---
+++ DP/0198_HouseRobber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+    if (nums.length == 0) return 0;
+    
+    int prev1 = 0;
+    int prev2 = 0;
+    for (int num: nums) {
+        int temp = prev1;
+        prev1 = Math.max(prev2 + num, prev1);
+        prev2 = temp;
+    }
+        return prev1;
+    }
+}
+
+// 2 7 6 3 5 1
