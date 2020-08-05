fd5300fb3775b53f308c213a66e62079c0babfe6
Brian Ho <hobr86@gmail.com>
2020-08-05 15:36:33 -0500

    Create 0658_FindKClosetElements.java

---
+++ BinarySearch/0658_FindKClosetElements.java
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/find-k-closest-elements/
+// the below solution works but is ugly. The idea is to find the left most qualified position.
+// to try another method
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        // 80 90 100 110 111 112
+        // k = 4 x = 95
+        
+        List<Integer> res = new LinkedList<>();
+        int r = 0;
+        for (int i = 0 ; i < arr.length; i++) {
+            if (x <= arr[i]) {
+                r = i;
+                break;
+            }
+        }
+        int l = r - 1;
+        int count = 0;
+        while (l >= 0 && count < k) {
+            if (x - arr[l] <= arr[r] - x) {
+                l--;
+                count++;
+            }
+            else {
+                r++;
+                count++;
+                if (r >= arr.length) {
+                    l -= k - count;
+                    break;
+                }
+            }
+        }
+        int j = 0;
+       while (j < k && l < arr.length - 1) {
+           res.add(arr[l+1]);
+           l++;
+           j++;
+       }
+        return res;
+    }
+}
